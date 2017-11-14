660eee8fe13599e07f227aa9116ff2eae67a99ca
monu <monubambroo@Hotmail.com>
2017-11-14 13:19:58 -0800

    commit

---
+++ WindowsFormsApp1/UnitTestProject2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5a07ddb2-0259-46fe-8aa0-6cc17d568a6b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WindowsFormsApp1/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Threading;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1 : SessionSetup
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            // Create session to launch a Calculator window
+            SessionSetup.Setup(context);
+
+        }
+
+    }
+    
+    public class SessionSetup
+    {
+        private const string AppId = @"C:\Users\monub\Desktop\WindowsFormsApp1\WindowsFormsApp1\WindowsFormsApp1\bin\Debug\WindowsFormsApp1.exe";
+        protected static WindowsDriver<WindowsElement> session;
+        private const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723/wd/hub";
+        
+
+        public static void Setup(TestContext context)
+        {
+            // Launch Calculator application if it is not yet launched
+            if (session == null)
+            {
+                // Create a new session to bring up an instance of the Calculator application
+                // Note: Multiple calculator windows (instances) share the same process Id
+                DesiredCapabilities appCapabilities = new DesiredCapabilities();
+                appCapabilities.SetCapability("app", AppId);
+                appCapabilities.SetCapability("deviceName", "WindowsPC");
+                session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities);
+                Assert.IsNotNull(session);
+                Assert.IsNotNull(session.SessionId);
+                // Set implicit timeout to 1.5 seconds to make element search to retry every 500 ms for at most three times
+                session.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(1.5));
+                // Verify that Notepad is started with untitled new file
+                // Keep track of the edit box to be used throughout the session
+                
+                session.FindElementByName("button1").Click();
+                
+                
+            }
+
+        }
+        public static void TearDown()
+        {
+            // Close the application and delete the session
+            if (session != null)
+            {
+                session.Quit();
+                session = null;
+            }
+        }
+
+    }
+
+    
+
+
+}
